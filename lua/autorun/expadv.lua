b948ac30f3d17002ede4272c62bfa7839cd66eb6
Rusketh <Rusketh@gmail.com>
2014-06-28 00:28:57 +0100

    Added von to repo. https://github.com/vercas/vON

--- lua/autorun/expadv.lua
+++ lua/autorun/expadv.lua
@@ -2,10 +2,12 @@ include( "expadv/core.lua" )
 
 if CLIENT then return end
 
+AddCSLuaFile( "expadv/context.lua" )
 AddCSLuaFile( "expadv/core.lua" )
 AddCSLuaFile( "expadv/components.lua" )
 AddCSLuaFile( "expadv/classes.lua" )
 AddCSLuaFile( "expadv/operators.lua" )
 AddCSLuaFile( "expadv/context.lua" )
-AddCSLuaFile( "expadv/context.lua" )
+AddCSLuaFile( "expadv/api/gcompute.lua" )
+AddCSLuaFile( "includes/modules/von.lua" )
 
