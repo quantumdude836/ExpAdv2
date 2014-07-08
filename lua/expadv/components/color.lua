3011f7e4a7dde6f6aeda2a738f06a59c70f231fb
Ian <nezz46@gmail.com>
2014-07-08 01:25:32 -0500

    Update color.lua

--- lua/expadv/components/color.lua
+++ lua/expadv/components/color.lua
@@ -77,36 +77,28 @@ EXPADV.AddFunctionAlias( "color", "n,n,n" )
 --GETTERS
 ColorComponent:AddInlineFunction( "getRed", "c:", "n", "@value 1.r")
 ColorComponent:AddFunctionHelper( "getRed", "c:", "Gets the red value of a color object")
-EXPADV.AddFunctionAlias( "getRed", "c:n" )
 
 ColorComponent:AddInlineFunction( "getGreen", "c:", "n", "@value 1.g")
 ColorComponent:AddFunctionHelper( "getGreen", "c:", "Gets the green value of a color object")
-EXPADV.AddFunctionAlias( "getGreen", "c:n" )
 
 ColorComponent:AddInlineFunction( "getBlue", "c:", "n", "@value 1.b")
 ColorComponent:AddFunctionHelper( "getBlue", "c:", "Gets the blue value of a color object")
-EXPADV.AddFunctionAlias( "getBlue", "c:n" )
 
 ColorComponent:AddInlineFunction( "getAlpha", "c:", "n", "@value 1.a")
 ColorComponent:AddFunctionHelper( "color", "c:", "Gets the alpha value of a color object")
-EXPADV.AddFunctionAlias( "getAlpha", "c:n" )
 
 --SETTERS
 ColorComponent:AddPreparedFunction( "setRed", "c:n", "", "@value 1.r = @value 2")
 ColorComponent:AddFunctionHelper( "setRed", "c:n", "Sets the red value of a color object")
-EXPADV.AddFunctionAlias( "setRed", "c:n" )
 
 ColorComponent:AddPreparedFunction( "setGreen", "c:n", "", "@value 1.g = @value 2")
 ColorComponent:AddFunctionHelper( "setGreen", "c:n", "Sets the green value of a color object")
-EXPADV.AddFunctionAlias( "setGreen", "c:n" )
 
 ColorComponent:AddPreparedFunction( "setBlue", "c:n", "", "@value 1.b = @value 2")
 ColorComponent:AddFunctionHelper( "setBlue", "c:n", "Sets the blue value of a color object")
-EXPADV.AddFunctionAlias( "setBlue", "c:n" )
 
 ColorComponent:AddPreparedFunction( "setAlpha", "c:n", "", "@value 1.a = @value 2")
 ColorComponent:AddFunctionHelper( "setAlpha", "c:n", "Sets the alpha value of a color object")
-EXPADV.AddFunctionAlias( "setAlpha", "c:n" )
 
 /* --- --------------------------------------------------------------------------------
 	@: HSV2RGB
@@ -114,8 +106,6 @@ EXPADV.AddFunctionAlias( "setAlpha", "c:n" )
 
 ColorComponent:AddInlineFunction( "hsv2rgb", "n,n,n", "c", "$HSVToColor(@value 1, @value 2, @value 3)")
 ColorComponent:AddFunctionHelper( "hsv2rgb", "n,n,n", "Converts hsv color to regular color")
-EXPADV.AddFunctionAlias( "hsv2rgb", "n,n,n" )
 
 ColorComponent:AddInlineFunction( "rgb2hsv", "n,n,n", "c", "$ColorToHSV(@value 1, @value 2, @value 3)")
 ColorComponent:AddFunctionHelper( "rgb2hsv", "n,n,n", "Converts regular color to hsv color")
-EXPADV.AddFunctionAlias( "rgb2hsv", "n,n,n" )
