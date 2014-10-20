2c8dfaabdde81ed91d7f142320cea387cb587013
szymski <szymski1@poczta.onet.pl>
2014-10-20 21:42:12 +0200

    Added some player functions

--- lua/expadv/components/player.lua
+++ lua/expadv/components/player.lua
@@ -52,6 +52,57 @@ Component:AddFunctionHelper( "isPlayer", "e:", "Returns true if the entity is a 
 Component:AddInlineFunction( "owner", "", "ply", "Context.player")
 Component:AddFunctionHelper( "owner", "", "Returns the owner of the gate.")
 
+Component:AddInlineFunction("isAdmin", "e:", "b", "(IsValid(@value 1) and @value 1:IsPlayer( ) and @value 1:IsAdmin( ))" )
+Component:AddInlineFunction("isSuperAdmin", "e:", "b", "(IsValid(@value 1) and @value 1:IsPlayer( ) and @value 1:IsSuperAdmin( ))" )
+
+Component:AddInlineFunction("team", "ply:", "n", "( (IsValid(@value 1) and @value 1:IsPlayer( )) and @value 1:Team( ) or 0 )" )
+Component:AddInlineFunction("teamName", "n", "s", "($team.GetName(@value 1) or \"\")" )
+Component:AddInlineFunction("teamScore", "n", "n", "($team.GetScore(@value 1) or 0)" )
+Component:AddInlineFunction("playersInTeam", "n", "n", "($team.NumPlayers(@value 1) or 0)" )
+Component:AddInlineFunction("teamDeaths", "n", "n", "($team.TotalDeaths(@value 1) or 0)" )
+Component:AddInlineFunction("teamFrags", "n", "n", "($team.TotalFrags(@value 1) or 0)" )
+Component:AddInlineFunction("teamColor", "n", "c", "$team.GetColor(@value 1)" )
+
+Component:AddInlineFunction("shootPos", "ply:", "v", "( IsValid(@value 1) and @value 1:GetShootPos() or Vector(0,0,0) )" )
+Component:AddInlineFunction("eye", "ply:", "v", "( IsValid(@value 1) and @value 1:GetAimVector() or @value 1:GetForward() or Vector(0,0,0)  )" )
+Component:AddInlineFunction("eyeAngles", "ply:", "a", "(IsValid(@value 1) and @value 1:EyeAngles() or Angle(0, 0, 0))" ) 
+Component:AddInlineFunction("aimEntity", "ply:", "e", "( IsValid(@value 1) and @value 1:GetEyeTraceNoCursor().Entity or Entity(0))" )
+Component:AddInlineFunction("aimNormal", "ply:", "v", "( IsValid(@value 1) and @value 1:GetEyeTraceNoCursor().HitNormal or Vector(0,0,0) )" )
+Component:AddInlineFunction("aimPos", "ply:", "v", "( IsValid(@value 1) and @value 1:GetEyeTraceNoCursor().HitPos or Vector(0,0,0) )" )
+
+Component:AddInlineFunction("steamID", "ply:", "s", "( IsValid(@value 1) and @value 1:SteamID() or \"\" )" )
+Component:AddInlineFunction("armor", "ply:", "n", "( IsValid(@value 1) and @value 1:Armor() or 0 )" )
+Component:AddInlineFunction("ping", "ply:", "n", "( IsValid(@value 1) and @value 1:Ping() or 0 )" )
+Component:AddInlineFunction("frags", "ply:", "n", "( IsValid(@value 1) and @value 1:Frags() or 0 )" )
+Component:AddInlineFunction("deaths", "ply:", "n", "( IsValid(@value 1) and @value 1:Deaths() or 0 )" )
+Component:AddInlineFunction("timeConnected", "ply:", "n", "( IsValid(@value 1) and @value 1:TimeConnected() or 0 )" )
+Component:AddInlineFunction("vehicle", "ply:", "e", "( IsValid(@value 1) and @value 1:GetVehicle() or Entity(0))" )
+Component:AddInlineFunction("inNoclip", "ply:", "b", "( IsValid(@value 1) and (@value 1:GetMoveType() == $MOVETYPE_NOCLIP) )" )
+Component:AddInlineFunction("flashLight", "ply:", "b", "(IsValid(@value 1) and @value 1:FlashlightIsOn( ))" )
+
+Component:AddInlineFunction("getEquipped", "ply:", "e", "(IsValid(@value 1) and (@value 1:GetActiveWeapon() or Entity(0)) or Entity(0) )" )
+
+local FuncKeys = {
+	["leftClick"] = IN_ATTACK,
+	["rightClick"] = IN_ATTACK2,
+	["keyForward"] = IN_FORWARD,
+	["keyLeft"] = IN_MOVELEFT,
+	["keyBack"] = IN_BACK,
+	["keyRight"] = IN_MOVERIGHT,
+	["keyJump"] = IN_JUMP,
+	["keyUse"] = IN_USE,IN_RELOAD,
+	["keyZoom"] = IN_ZOOM,
+	["keyWalk"] = IN_WALK,
+	["keySprint"] = IN_SPEED,
+	["keyDuck"] = IN_DUCK,
+	["keyLeftTurn"] = IN_LEFT,
+	["keyRightTurn"] = IN_RIGHT,
+}
+
+for Name, Enum in pairs( FuncKeys ) do
+	Component:AddInlineFunction( Name, "ply:", "b", "(IsValid(@value 1) and @value 1:KeyDown( " .. Enum .. " ) )" )
+end
+
 Component:AddVMFunction( "players", "", "t", 
 	function( Context, Trace )
 		local Data, Types, Look, I = { }, { }, { }, 0
