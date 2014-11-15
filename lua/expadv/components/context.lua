00406a1b79b6668bc3e0602ded0a65227b1d51bf
Ripmax <siranderson66@gmail.com>
2014-11-15 17:04:56 +0000

    Added ability to remove options

--- lua/expadv/components/context.lua
+++ lua/expadv/components/context.lua
@@ -12,8 +12,11 @@ function Component:OnRegisterContext(Context) Context.Data.CustomMenus = {} end
 
 EXPADV.ClientOperators()
 
-Component:AddPreparedFunction("addContextMenu", "s,d", "", [[Context.Data.CustomMenus[string.Trim(string.lower(@value 1))] = {Name = @value 1, Callback = @value 2}]])
-Component:AddFunctionHelper("addContextMenu", "s,d", "Adds an option to the chip's context menu.")
+Component:AddPreparedFunction("addContextMenu", "s,s,d", "", [[Context.Data.CustomMenus[@value 1] = {Name = @value 2, Callback = @value 3}]])
+Component:AddFunctionHelper("addContextMenu", "s,s,d", "Adds an option to the chip's context menu.")
+
+Component:AddPreparedFunction("removeContextMenu", "s", "", [[table.remove(Context.Data.CustomMenus, @value 1]])
+Component:AddFunctionHelper("removeContextMenu", "s", "Removes a custom option from the chip's context menu.")
 
 function Component:OnOpenContextMenu( Entity, Menu, Trace, Option )
 	local Context = Entity.Context
