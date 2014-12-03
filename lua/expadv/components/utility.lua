21f53d2ce68eea5d4f904e6de25fb6cc21f1366f
Rusketh <Rusketh@gmail.com>
2014-12-03 19:15:14 +0000

    Added: Unit convert functions to the utility component.

--- lua/expadv/components/utility.lua
+++ lua/expadv/components/utility.lua
@@ -164,6 +164,115 @@ Component:AddInlineFunction( "time", "s", "n", "$tonumber( $os.date(\"!*t\")[ @v
 Component:AddFunctionHelper( "time", "s", "Returns the current time is unit S." )
 
 /* --- --------------------------------------------------------------------------------
+	@: Apparently we need unti convershion
+   --- */
+local UnitSpeed = {
+	["u/s"] = 1 / 0.75,
+	["u/m"] = 60 * (1 / 0.75),
+	["u/h"] = 3600 * (1 / 0.75),
+
+	["mm/s"] = 25.4,
+	["cm/s"] = 2.54,
+	["dm/s"] = 0.254,
+	["m/s"] = 0.0254,
+	["km/s"] = 0.0000254,
+	["in/s"] = 1,
+	["ft/s"] = 1 / 12,
+	["yd/s"] = 1 / 36,
+	["mi/s"] = 1 / 63360,
+	["nmi/s"] = 127 / 9260000,
+
+	["mm/m"] = 60 * 25.4,
+	["cm/m"] = 60 * 2.54,
+	["dm/m"] = 60 * 0.254,
+	["m/m"] = 60 * 0.0254,
+	["km/m"] = 60 * 0.0000254,
+	["in/m"] = 60,
+	["ft/m"] = 60 / 12,
+	["yd/m"] = 60 / 36,
+	["mi/m"] = 60 / 63360,
+	["nmi/m"] = 60 * 127 / 9260000,
+
+	["mm/h"] = 3600 * 25.4,
+	["cm/h"] = 3600 * 2.54,
+	["dm/h"] = 3600 * 0.254,
+	["m/h"] = 3600 * 0.0254,
+	["km/h"] = 3600 * 0.0000254,
+	["in/h"] = 3600,
+	["ft/h"] = 3600 / 12,
+	["yd/h"] = 3600 / 36,
+	["mi/h"] = 3600 / 63360,
+	["nmi/h"] = 3600 * 127 / 9260000,
+
+	["mph"] = 3600 / 63360,
+	["knots"] = 3600 * 127 / 9260000,
+	["mach"] = 0.0254 / 295,
+}
+
+local UnitLength = {
+	["u"] = 1 / 0.75,
+
+	["mm"] = 25.4,
+	["cm"] = 2.54,
+	["dm"] = 0.254,
+	["m"] = 0.0254,
+	["km"] = 0.0000254,
+	["in"] = 1,
+	["ft"] = 1 / 12,
+	["yd"] = 1 / 36,
+	["mi"] = 1 / 63360,
+	["nmi"] = 127 / 9260000,
+}
+
+local UnitWeight = {
+	["g"] = 1000,
+	["kg"] = 1,
+	["t"] = 0.001,
+	["oz"] = 1 / 0.028349523125,
+	["lb"] = 1 / 0.45359237,
+}
+
+Component:AddVMFunction( "toUnit", "s,n", "n",
+	function( Context, Trace, Unit, Value )
+		if UnitSpeed[Unit] then
+			return (Value * 0.75) * UnitSpeed[Unit]
+		elseif UnitLength[Unit] then
+			return (Value * 0.75) * UnitLength[Unit]
+		elseif UnitWeight[Unit] then
+			return Value * UnitWeight[Unit]
+		end
+
+		return -1
+	end )
+
+Component:AddVMFunction( "fromUnit", "s,n", "n",
+	function( Context, Trace, Unit, Value )
+		if UnitSpeed[Unit] then
+			return (Value / 0.75) / UnitSpeed[Unit]
+		elseif UnitLength[Unit] then
+			return (Value / 0.75) / UnitLength[Unit]
+		elseif UnitWeight[Unit] then
+			return Value / UnitWeight[Unit]
+		end
+
+		return -1
+	end )
+
+
+Component:AddVMFunction( "convertUnit", "s,s,n", "n",
+	function( Context, Trace, To, From, Value )
+		if UnitSpeed[To] and UnitSpeed[From] then
+			return Value * (UnitSpeed[From] / UnitSpeed[To])
+		elseif UnitLength[To] and UnitLength[From] then
+			return Value * (UnitLength[From] / UnitLength[To])
+		elseif UnitWeight[To] and UnitWeight[From] then
+			return Value * (UnitWeight[From] / UnitWeight[To])
+		end
+
+		return -1
+	end )
+
+/* --- --------------------------------------------------------------------------------
 	@: Some useful array sorting functions
    --- */
 
